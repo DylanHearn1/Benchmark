e3b45e3de49939e73689db6ba856a72ecb10d102
Dylan Hearn <dylanhearn123@gmail.com>
2024-03-25 08:44:31 +0000

    feature: leaderboards are now visible

--- src/components/SingleGame.tsx
+++ src/components/SingleGame.tsx
@@ -1,19 +1,26 @@
 import { ReactElement } from 'react';
 import Navbar from './Navbar';
+import { highScoresInterface } from '../pages/PatternMemoryPage';
 
 interface SingleGameProps {
   title: string;
   description: string;
   game: ReactElement;
+  highscores: Array<highScoresInterface>;
 }
-const SingleGame = ({ title, description, game }: SingleGameProps) => {
+const SingleGame = ({
+  title,
+  description,
+  game,
+  highscores,
+}: SingleGameProps) => {
   return (
     <>
       <header>
         <Navbar />
       </header>
-      <main className="text-p">
-        <div className="bg-black h-dvh text-white flex justify-center items-center">
+      <main className="text-p bg-black text-white">
+        <div className="h-dvh  flex justify-center items-center">
           <div className="space-y-10">
             <div className="text-center">
               <p>{title}</p>
@@ -22,6 +29,12 @@ const SingleGame = ({ title, description, game }: SingleGameProps) => {
             {game}
           </div>
         </div>
+        {highscores.map((user, index) => (
+          <div key={index}>
+            <p>{user.name}</p>
+            <p>{user.patternMemory}</p>
+          </div>
+        ))}
       </main>
     </>
   );

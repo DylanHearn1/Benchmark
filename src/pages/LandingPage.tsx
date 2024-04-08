12271c591880f66f74f7f5f3473987e7418d2623
Dylan Hearn <dylanhearn123@gmail.com>
2024-04-08 03:20:49 +0100

    ui feature to suggest scroll content

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,10 +8,13 @@ const LandingPage = () => {
         <Navbar />
       </header>
       <main>
-        <div className="bg-black min-h-dvh flex items-center text-5xl text-white w-full fixed">
+        <div className="bg-black min-h-dvh flex flex-col justify-center items-center text-5xl text-white w-full fixed">
           <div className="w-1280px mx-auto text-center">
-            <p className="font-semibold">Human</p>
+            <p className="font-semibold">Brain</p>
             <span className="text-gradient font-semibold">Benchmark</span>
+          </div>
+          <div className="absolute bottom-8 w-40 h-12 animate-bounce">
+            <div className="w-full h-1.5 bg-white rounded-full opacity-75 absolute bottom-0"></div>
           </div>
         </div>
         <div className="min-h-dvh flex flex-col top-screen absolute bg-black border-top-2 border-slate-800 rounded-3xl w-full">

566af4fe78caae5dc8fcadcc61d97aace7441567
Dylan Hearn <dylanhearn123@gmail.com>
2024-04-11 06:09:39 +0100

    style: logged in users, username bold leaderboard

--- src/components/TopThreeScore.tsx
+++ src/components/TopThreeScore.tsx
@@ -1,3 +1,5 @@
+import { useAuthContext } from '../hooks/useAuthContext';
+
 interface TopThreeScoreProps {
   name: string;
   score: number;
@@ -5,11 +7,19 @@ interface TopThreeScoreProps {
 }
 
 const TopThreeScore = ({ name, score, position }: TopThreeScoreProps) => {
+  const { username } = useAuthContext();
+
   return (
     <div className="text-white text-center justify-center border-gradient px-5 py-10 md:px-10 md:py-20 mb-10">
       <p className="pb-20 text-2xl font-bold">#{position}</p>
       <div className="flex justify-between flex-wrap">
-        <p>{name}</p>
+        <p
+          className={
+            username === name ? 'underline underline-offset-4 font-bold' : ''
+          }
+        >
+          {name}
+        </p>
         <h1>SCORE: {score}</h1>
       </div>
     </div>
